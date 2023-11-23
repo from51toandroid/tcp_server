9d01630781cea6a6da7dc01514382663446b8709
daipan <daipan@youhuatech.com>
2023-11-23 19:36:16 +0800

    格式代码

--- htfsk.h
+++ htfsk.h
@@ -10,34 +10,13 @@ typedef void * (* func_pointer)(void *arg);
 
 
 
-
-
 #define DEV_PATH_POWER_STRIKE    "/dev/input/event1"
 #define DEV_PATH2                "/dev/input/event2"
 #define DEV_PATH_TOUCH_SCREEN    "/dev/input/event3"
-
-#define VERSION_FILE_PATH "/system/tempdir/tmp_version.txt"
-#define VERSION_DESCRIPTION_MATCH_STR "[ro.product.firmware]: [v"
-#define VERSION_ANDROID_FIRST 210
-
-#define VERSION_APK_DESCRIPTION_MATCH_STR "[persist.sys.apk.version]: [V"
-//1.2.0_2]
-
-
-
-#define RELEASE_APK_NAME "xxx.apk"
-#define RELEASE_APK_BACKUP_PATH "/system/etc/init.d/xxx_new.apk"
-#define RELEASE_APK_OLD_PATH    "/system/etc/init.d/xxx.apk"
-
-
 #define OLD_APK_RES_BACKUP_PATH   "/data/data/xxx/files/"
-
-
-#define REINSTALL_APK_PATH "pm install -r /data/data/xxx/files/_update/xxx.apk"
-#define REINSTALL_APK_PATH_LEN strlen("pm install -r /data/data/xxx/files/_update/xxx.apk") 
+#define REINSTALL_APK_PATH_LEN strlen("pm install -r /data/a.apk") 
 
 #define DAEMON_VERSION   "V1.03"
-
 
 #define TAG  "htfsk"
 #define RELEASE_VERSION 1 
